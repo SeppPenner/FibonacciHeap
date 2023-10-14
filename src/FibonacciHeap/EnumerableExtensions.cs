69f2f48be3f46bacb14d8150b2d319362ae55350
SeppPenner <HansM2013@gmx.de>
2023-10-14 19:54:13 +0200

    Removed invalid copyright year.

--- src/FibonacciHeap/EnumerableExtensions.cs
+++ src/FibonacciHeap/EnumerableExtensions.cs
@@ -1,6 +1,6 @@
 // --------------------------------------------------------------------------------------------------------------------
 // <copyright file="EnumerableExtensions.cs" company="Hämmer Electronics">
-//   Copyright (c) 2020 All rights reserved.
+//   Copyright (c) All rights reserved.
 // </copyright>
 // <summary>
 //   This class contains <see cref="IEnumerable{T}"/> extension methods.
